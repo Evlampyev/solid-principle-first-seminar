e63ae1f0c2a7e4a8802e743686cfc23a535a1165
Danil Iarosh <maze3290@gmail.com>
2023-03-20 22:14:16 +0300

    lesson 6 changes

--- src/dip1/Main.java
+++ src/dip1/Main.java
@@ -1,10 +1,14 @@
 package dip1;
 
 import dip1.model.Report;
+import dip1.model.ReportItem;
+import dip1.model.util.Printer;
+import dip1.model.util.ReportPrinter;
 
 public class Main {
     public static void main(String[] args) {
-        Report report = new Report();
+        Printer<ReportItem> printer = new ReportPrinter();
+        Report report = new Report(printer);
         report.calculate();
         report.output();
     }
